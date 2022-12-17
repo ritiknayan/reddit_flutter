022153f5808cb4f577611713302d9c422cc98f78
Ritik Nayan <ritikn4546@gmail.com>
2022-12-17 21:29:32 +0530

    Setup firebase

---
+++ lib/core/common/sign_in_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:redditflutter/core/constants.dart';
+import 'package:redditflutter/theme/pallete.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton.icon(
+        onPressed: () {},
+        icon: Image.asset(
+          Constants.googlePath,
+          width: 35,
+        ),
+        label: const Text(
+          'Continue with Google',
+          style: TextStyle(fontSize: 18),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Pallete.greyColor,
+          minimumSize: const Size(
+            double.infinity,
+            50,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
